2e48d484e364dac0325d527d0147a3c739ff4745
Maycon Wendell <mayconwendell564@gmail.com>
2026-05-11 15:41:38 -0300

    feat: adiciona relatório financeiro e melhora organização

--- financeiro.py
+++ financeiro.py
@@ -1,67 +1,20 @@
-def adicionar_movimentacao(tipo, movimentacoes):
-<<<<<<< HEAD
-    
-=======
+def adicionar(tipo, movimentacoes):
 
->>>>>>> 91677ab (feat: adiciona relatório financeiro)
     continuar = 's'
 
     while continuar == 's':
 
         try:
             nome = input('Digite o nome: ')
-<<<<<<< HEAD
-            print("\n========== Categorias ==========\n")
-            print("1 - Alimentação" \
-            "\n2 - Transporte" \
-            "\n3 - Lazer" \
-            "\n4 - Salário" \
-            "\n5 - Estudos" \
-            "\n6 - Outro")
-            categoria = int(input('Qual categoria: '))
-
-            if categoria == 1:
-                categoria = 'alimentação'
-            
-            elif categoria == 2:
-                categoria = 'transporte'
-            
-            elif categoria == 3:
-                categoria = 'lazer'
-            
-            elif categoria == 4:
-                categoria = 'salario'
-            
-            elif categoria == 5:
-                categoria = 'estudos'
-            
-            elif categoria == 6:
-                nova_categoria = input('Digite a categoria: ').lower()
-                categoria = nova_categoria
-
-            else:
-                print("Categoria inválida!")
-                
-            valor = float(input('Digite o valor: '))
-
-            nova_movimentacao = {
-                'nome': nome,
-                'valor': valor,
-                'tipo': tipo,
-                'categoria': categoria
-            }
-=======
 
             print("\n========== Categorias ==========\n")
-            print(
-                "1 - Alimentação"
-                "\n2 - Transporte"
-                "\n3 - Lazer"
-                "\n4 - Salário"
-                "\n5 - Estudos"
-                "\n6 - Outro"
-            )
->>>>>>> 91677ab (feat: adiciona relatório financeiro)
+
+            print("1 - Alimentação"
+                  "\n2 - Transporte"
+                  "\n3 - Lazer"
+                  "\n4 - Salário"
+                  "\n5 - Estudos"
+                  "\n6 - Outro")
 
             categoria = int(input('Qual categoria: '))
             categoria_valida = True
@@ -106,76 +59,28 @@ def adicionar_movimentacao(tipo, movimentacoes):
         except ValueError:
             print('Você digitou um valor inválido!')
 
-        continuar = input(
-            'Deseja adicionar outra movimentação? (s / n) '
-        ).lower().strip()
+        continuar = input('Deseja adicionar outra movimentação? (s / n) ').lower().strip()
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 91677ab (feat: adiciona relatório financeiro)
 def ver_movimentacoes(movimentacoes):
 
     if not movimentacoes:
         print('Não tem movimentações cadastradas!')
         return
 
-<<<<<<< HEAD
-    else:
-        try:
-            continuar = 's'
-        
-            print('\n1 - Ver Tudo'
-            '\n2 - Ver Receitas'
-            '\n3 - Ver despesas'
-            '\n4 - Voltar\n')
-                
-            while continuar == 's':
-=======
-    print(
-        '\n1 - Ver Tudo'
-        '\n2 - Ver Receitas'
-        '\n3 - Ver Despesas'
-        '\n4 - Voltar\n'
-    )
-
     try:
 
         continuar = 's'
 
         while continuar == 's':
->>>>>>> 91677ab (feat: adiciona relatório financeiro)
 
-                pergunta = int(input('Qual opção deseja: '))
+            print('\n1 - Ver Tudo'
+                  '\n2 - Ver Receitas'
+                  '\n3 - Ver Despesas'
+                  '\n4 - Voltar\n')
 
-<<<<<<< HEAD
-                if pergunta < 1 or pergunta > 4:
-                        print('Opção inválida!')
+            pergunta = int(input('Qual opção deseja: '))
 
-                elif pergunta == 4:
-                    return
-                
-                else:
-
-                    for i, movimentacao in enumerate(movimentacoes):
-        
-                        if pergunta == 1:
-                            print(f"{i + 1} - {movimentacao['nome']} - {movimentacao['valor']} - {movimentacao['tipo']}")
-                        
-                        elif pergunta == 2:
-                            if movimentacao['tipo'] == 'receita':
-
-                                print(f"{i + 1} - {movimentacao['nome']} - {movimentacao['valor']} - Receita")
-
-                        elif pergunta == 3:
-                            if movimentacao['tipo'] == 'despesa':
-                                print(f"{i + 1} - {movimentacao['nome']} - {movimentacao['valor']} - Despesa")
-
-        except ValueError:
-            print('Você digitou um valor inválido!')
-
-        continuar = input('Deseja escolher outra opção? (s / n) ').lower().strip()
-=======
             if pergunta < 1 or pergunta > 4:
                 print('Opção inválida!')
 
@@ -187,66 +92,30 @@ def ver_movimentacoes(movimentacoes):
                 for i, movimentacao in enumerate(movimentacoes):
 
                     if pergunta == 1:
-                        print(
-                            f"{i + 1} - "
-                            f"{movimentacao['nome']} - "
-                            f"R$ {movimentacao['valor']} - "
-                            f"{movimentacao['tipo']} - "
-                            f"{movimentacao['categoria']}"
-                        )
+
+                        print(f"{i + 1} - {movimentacao['nome']} - R$ {movimentacao['valor']:.2f} - {movimentacao['tipo']} - {movimentacao['categoria']}")
 
                     elif pergunta == 2:
 
                         if movimentacao['tipo'] == 'receita':
 
-                            print(
-                                f"{i + 1} - "
-                                f"{movimentacao['nome']} - "
-                                f"R$ {movimentacao['valor']} - "
-                                f"Receita - "
-                                f"{movimentacao['categoria']}"
-                            )
-
+                            print(f"{i + 1} - {movimentacao['nome']} - R$ {movimentacao['valor']:.2f} - Receita - {movimentacao['categoria']}")
                     elif pergunta == 3:
 
                         if movimentacao['tipo'] == 'despesa':
 
-                            print(
-                                f"{i + 1} - "
-                                f"{movimentacao['nome']} - "
-                                f"R$ {movimentacao['valor']} - "
-                                f"Despesa - "
-                                f"{movimentacao['categoria']}"
-                            )
+                            print(f"{i + 1} - {movimentacao['nome']} - R$ {movimentacao['valor']:.2f} - Despesa - {movimentacao['categoria']}")
 
-            continuar = input(
-                '\nDeseja escolher outra opção? (s / n) '
-            ).lower().strip()
+            continuar = input('\nDeseja escolher outra opção? (s / n) ').lower().strip()
 
     except ValueError:
         print('Você digitou um valor inválido!')
 
->>>>>>> 91677ab (feat: adiciona relatório financeiro)
 
 def ver_saldo(movimentacoes):
 
     if not movimentacoes:
         print('Não tem movimentações cadastradas!')
-<<<<<<< HEAD
-    
-    else:
-        somar = 0
-
-        for movimentacao in movimentacoes:
-            
-            if movimentacao['tipo'] == 'receita':
-                somar += movimentacao['valor']
-            
-            elif movimentacao['tipo'] == 'despesa':
-                somar -= movimentacao['valor']
-            
-        print(f'Seu saldo final é R$: {somar}')
-=======
         return
 
     saldo = 0
@@ -261,27 +130,28 @@ def ver_saldo(movimentacoes):
 
     print(f'Seu saldo final é R$: {saldo:.2f}')
 
+
 def relatorio(movimentacoes):
+
     if not movimentacoes:
         print('Não tem movimentações cadastradas!')
-    
-    else:        
-        
-        total_rec = 0
-        total_des = 0
+        return
 
-        print('========== RELATÓRIO ==========')
-        for movimentacao in movimentacoes:
-            if movimentacao['tipo'] == 'receita':
-                total_rec += movimentacao['valor']
+    total_rec = 0
+    total_des = 0
 
-            elif movimentacao['tipo'] == 'despesa':
-                total_des += movimentacao['valor']
+    print('\n========== RELATÓRIO ==========\n')
 
-        saldo = total_rec - total_des
+    for movimentacao in movimentacoes:
 
-        print(f'Total de receitas: R$ {total_rec:.2f}')
-        print(f'Total de despesas: R$ {total_des:.2f}')
-        print(f'Saldo final: R$ {saldo:.2f}') 
-        
->>>>>>> 91677ab (feat: adiciona relatório financeiro)
+        if movimentacao['tipo'] == 'receita':
+            total_rec += movimentacao['valor']
+
+        elif movimentacao['tipo'] == 'despesa':
+            total_des += movimentacao['valor']
+
+    saldo = total_rec - total_des
+
+    print(f'Total de receitas: R$ {total_rec:.2f}')
+    print(f'Total de despesas: R$ {total_des:.2f}')
+    print(f'Saldo final: R$ {saldo:.2f}')
--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-from jso_utils import salvar, carregar
-from financeiro import adicionar_movimentacao, ver_movimentacoes, ver_saldo
-
-def menu(movimentacoes):
-=======
 from financeiro import (
-    adicionar_movimentacao,
+    adicionar,
     ver_movimentacoes,
     ver_saldo,
     relatorio
@@ -14,46 +8,34 @@ from financeiro import (
 from json_utils import salvar, carregar
 
 
-def menu():
->>>>>>> 91677ab (feat: adiciona relatório financeiro)
-
+def menu(movimentacoes):
     while True:
-
         print('\n========== MENU ==========\n')
 
         print(
             '1 - Adicionar Receita'
             '\n2 - Adicionar Despesa'
             '\n3 - Ver Movimentações'
-            '\n4 - Ver Saldo' 
+            '\n4 - Ver Saldo'
             '\n5 - Ver Relatório'
-            '\n5 - Sair\n'
+            '\n6 - Sair\n'
         )
 
         try:
-
             opcao = int(input('Qual opção deseja: '))
 
             if opcao == 1:
-                adicionar_movimentacao('receita', movimentacoes)
-<<<<<<< HEAD
-            
-=======
+                adicionar('receita', movimentacoes)
 
->>>>>>> 91677ab (feat: adiciona relatório financeiro)
             elif opcao == 2:
-                adicionar_movimentacao('despesa', movimentacoes)
+                adicionar('despesa', movimentacoes)
 
             elif opcao == 3:
                 ver_movimentacoes(movimentacoes)
 
             elif opcao == 4:
                 ver_saldo(movimentacoes)
-<<<<<<< HEAD
-            
-=======
 
->>>>>>> 91677ab (feat: adiciona relatório financeiro)
             elif opcao == 5:
                 relatorio(movimentacoes)
 
@@ -68,12 +50,6 @@ def menu():
         except ValueError:
             print('Você digitou um valor inválido!')
 
-<<<<<<< HEAD
-movimentacoes = carregar()
-menu()
-=======
 
 movimentacoes = carregar()
-
 menu()
->>>>>>> 91677ab (feat: adiciona relatório financeiro)
